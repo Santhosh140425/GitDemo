f1bdff78bcaf481709f3f961a21ead8d4a35ec5a
santhosh <santhoshu@softclouds.com>
2025-05-21 15:13:05 +0530

    initial version

---
+++ src/test/java/org/test/GoogleTest.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+public class GoogleTest {
+
+    @Test
+    public void san(){
+        open("https://www.google.com");
+        System.out.println("Done");
+    }
+}
